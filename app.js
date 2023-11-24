3a7bae43816606929c8e4f0b56548df8ec834ef9
Victor-Net <84307620+Victor-Net@users.noreply.github.com>
2023-11-24 20:16:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cardsAr = [
     card: 1,
     title: "Portfolio",
     img: "./images/portfolio.png",
-    link: "https://victor-net.github.io/portfolio/",
+    link: href="#home",
     text: "This is the site with the projects I have done.",
   },
   {
