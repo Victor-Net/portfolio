11f6ee1060dc04221f4ef73d9eefcadce5b881c2
Victor-Net <84307620+Victor-Net@users.noreply.github.com>
2023-11-25 21:01:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 // objects
 const cardsAr = [
   {
-    card: 1,
+    card: "portfolio",
     title: "Portfolio",
     img: "./images/portfolio.png",
     link: "",
     text: "This is the site with the projects I have done.",
   },
   {
-    card: 2,
-    title: "ACK Menu",
+    card: "menu",
+    title: "Menu",
     img: "./images/ack.png",
-    link: `https://victor-net.github.io/js-menu/`,
+    link: `https://victor-net.github.io/menu/`,
     text: "A menu for a restaurant or cafe. Built using JavaScript, HTML and CSS",
   },
   {
-    card: 3,
+    card: "weather",
     title: "Weather App",
-    img: "./images/weather.png",
+    img: "./images/react.png",
     link: "",
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe, itaque doloremque tempora quia veniam expedita.",
   },
   {
-    card: 4,
+    card: "hand-clock-app",
     title: "Hand Clock App",
     img: "./images/clock.png",
-    link: "https://victor-net.github.io/hand-clock-app/",
+    link: "",
     text: "Simple hand clock utilizing HTML, CSS and JavaScript.",
   },
   {
-    card: 5,
+    card: "coffee",
     title: "Coffee Shop",
     img: "./images/coffee-shop.png",
-    link: "https://victor-net.github.io/coffeebs5/",
+    link: "",
     text: "This is a simple website just to showcase some knowledge of Bootstrap.",
   },
 ];
@@ -40,19 +40,19 @@ const cardsAr = [
 // target where to display the cards i.e .projects
 const projectsEl = document.querySelector(".projects");
 const card = document.querySelector(".cards");
-const timeEl = document.getElementById("time")
+const timeEl = document.querySelector(".time");
 timeEl.innerText = new Date().toDateString();
 
 // Event listener for when dom is loaded
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", () => {
   showCards();
   console.log("DOMContentLoaded");
 });
 
 // display cards function
 const showCards = () => {
-  let showCard = cardsAr.map(function (card) {
-    return `<div class="card">
+  let showCard = cardsAr.map((card) => {
+    return `<div class="card ${card.card}">
         <img src=${card.img} alt=${card.alt}/>
         <div class="card-info">
           <h3>${card.title}</h3>
@@ -65,11 +65,12 @@ const showCards = () => {
   });
   showCard = showCard.join(""); // join method fixes the comma issue after every card
   projectsEl.innerHTML = showCard;
+
+  // To remove link from portfolio
+  // if (card.classList.contains("portfolio")) {
+  //   card.classList.add()
+  // }
 };
 
 let date = document.querySelector(".date");
 date.innerText = new Date().getFullYear();
-
-//
-// strt 10nov15th23wed
-// end 22:45nov15th23wed
